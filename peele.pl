09e9be0908d7690033b7ac6e1df8a317bfe045d8
Vlad <alzwded@gmail.com>
2014-06-01 21:29:41 +0300

    add stub peele.pl

---
+++ peele.pl
@@ -0,0 +1,7 @@
+# TODO
+# outline:
+#   process command line args (load document, set additional plugin paths)
+#   set up document model
+#   foreach (@pluginPath) { PluginManager::load($_); }
+#   show window
+#   main loop
